4b43f7d06cb4f04f9da63380e303f87d6bfc5e44
Kaushik H S <kaushik0h0s@gmail.com>
2025-07-10 22:02:31 +0530

    restructuring files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { Server } from 'lucide-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {   
+    proxy: {
+      '/v1/api': 'http://localhost:8000',
+    },
   },
 });
