37ad13ead59bc52a6a346654f300189da8d0792e
Kaushik H S <kaushik0h0s@gmail.com>
2025-08-05 23:49:10 +0530

    adding leetcodescheduler to schedule refresh stats

---
+++ backend/src/utils/leetcodeScheduler.js
@@ -0,0 +1,12 @@
+import cron from "node-cron";
+import { refreshAllLeetcodeStats } from "../controllers/leetcodeleaderboard.controller.js";
+export function startLeetcodeStatsScheduler() {
+  cron.schedule("0 * * * *", async () => {
+    await refreshAllLeetcodeStats();
+    console.log("Hourly leetcode stats refresh complete.");
+  });
+  setTimeout(async () => {
+    await refreshAllLeetcodeStats();
+    console.log("Initial leetcode stats refresh complete.");
+  }, 1000); 
+}
